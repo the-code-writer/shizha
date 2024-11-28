06da1f08d1685c7136bfdfd4bf4be781ff2b9122
Douglas Maposa <dovelous@gmail.com>
2024-11-28 21:30:54 +0200

    update data 1

---
+++ starter-kit/src/data/bulawayo.ts
@@ -0,0 +1,212 @@
+const bulawayoCities = [
+    {
+        "name": "Bulawayo",
+        "province": "Bulawayo",
+        "latitude": -20.0012,
+        "longitude": 28.6295
+    },
+    {
+        "name": "Barham Green",
+        "province": "Bulawayo",
+        "latitude": -20.0723,
+        "longitude": 28.5856
+    },
+    {
+        "name": "Beacon Hill",
+        "province": "Bulawayo",
+        "latitude": -20.0525,
+        "longitude": 28.6283
+    },
+    {
+        "name": "Bellevue",
+        "province": "Bulawayo",
+        "latitude": -20.0582,
+        "longitude": 28.5900
+    },
+    {
+        "name": "Belmont",
+        "province": "Bulawayo",
+        "latitude": -20.0528,
+        "longitude": 28.5781
+    },
+    {
+        "name": "Bradfield",
+        "province": "Bulawayo",
+        "latitude": -20.0194,
+        "longitude": 28.5639
+    },
+    {
+        "name": "Burnside",
+        "province": "Bulawayo",
+        "latitude": -20.0511,
+        "longitude": 28.6064
+    },
+    {
+        "name": "Cowdray Park",
+        "province": "Bulawayo",
+        "latitude": -20.0634,
+        "longitude": 28.6041
+    },
+    {
+        "name": "Entumbane",
+        "province": "Bulawayo",
+        "latitude": -20.0487,
+        "longitude": 28.6205
+    },
+    {
+        "name": "Famona",
+        "province": "Bulawayo",
+        "latitude": -20.0146,
+        "longitude": 28.5701
+    }
+];
+
+const harareSuburbs = [
+    {
+        "name": "Avondale",
+        "province": "Harare",
+        "latitude": -17.8228,
+        "longitude": 31.0323
+    },
+    {
+        "name": "Borrowdale",
+        "province": "Harare",
+        "latitude": -17.7546,
+        "longitude": 31.0484
+    },
+    {
+        "name": "Belgravia",
+        "province": "Harare",
+        "latitude": -17.8250,
+        "longitude": 31.0362
+    },
+    {
+        "name": "Greendale",
+        "province": "Harare",
+        "latitude": -17.8221,
+        "longitude": 31.0588
+    },
+    {
+        "name": "Mount Pleasant",
+        "province": "Harare",
+        "latitude": -17.8075,
+        "longitude": 31.0613
+    },
+    {
+        "name": "Highlands",
+        "province": "Harare",
+        "latitude": -17.7628,
+        "longitude": 31.0797
+    },
+    {
+        "name": "Eastlea",
+        "province": "Harare",
+        "latitude": -17.8279,
+        "longitude": 31.0562
+    },
+    {
+        "name": "Mabvuku",
+        "province": "Harare",
+        "latitude": -17.9721,
+        "longitude": 31.0611
+    },
+    {
+        "name": "Waterfalls",
+        "province": "Harare",
+        "latitude": -17.9733,
+        "longitude": 31.0594
+    },
+    {
+        "name": "Emerald Hill",
+        "province": "Harare",
+        "latitude": -17.9792,
+        "longitude": 31.0278
+    },
+    {
+        "name": "Chisipite",
+        "province": "Harare",
+        "latitude": -17.7591,
+        "longitude": 31.0526
+    },
+    {
+        "name": "Kuwadzana",
+        "province": "Harare",
+        "latitude": -17.9628,
+        "longitude": 31.0614
+    }
+];
+
+const mashonalandWestCities = [
+    {
+        "name": "Chinhoyi",
+        "province": "Mashonaland West",
+        "latitude": -17.3667,
+        "longitude": 30.2000
+    },
+    {
+        "name": "Kadoma",
+        "province": "Mashonaland West",
+        "latitude": -19.1965,
+        "longitude": 29.9131
+    },
+    {
+        "name": "Chegutu",
+        "province": "Mashonaland West",
+        "latitude": -18.1803,
+        "longitude": 29.7459
+    },
+    {
+        "name": "Kariba",
+        "province": "Mashonaland West",
+        "latitude": -16.5455,
+        "longitude": 28.8255
+    },
+    {
+        "name": "Norton",
+        "province": "Mashonaland West",
+        "latitude": -17.9828,
+        "longitude": 30.5564
+    },
+    {
+        "name": "Hurungwe",
+        "province": "Mashonaland West",
+        "latitude": -16.7986,
+        "longitude": 29.5844
+    },
+    {
+        "name": "Makonde",
+        "province": "Mashonaland West",
+        "latitude": -16.8539,
+        "longitude": 30.0211
+    },
+    {
+        "name": "Mhondoro-Ngezi",
+        "province": "Mashonaland West",
+        "latitude": -18.0956,
+        "longitude": 29.7632
+    },
+    {
+        "name": "Sanyati",
+        "province": "Mashonaland West",
+        "latitude": -17.0000,
+        "longitude": 30.5000
+    },
+    {
+        "name": "Zvimba",
+        "province": "Mashonaland West",
+        "latitude": -17.4077,
+        "longitude": 30.0972
+    },
+    {
+        "name": "Karoi",
+        "province": "Mashonaland West",
+        "latitude": -16.8181,
+        "longitude": 29.7408
+    },
+    {
+        "name": "Gadzema",
+        "province": "Mashonaland West",
+        "latitude": -18.0422,
+        "longitude": 30.1597
+    }
+];
