e538384cd95ea2ce96c9fa8cb0dc8b8e55a26ad6
Douglas Maposa <dovelous@gmail.com>
2024-11-28 21:03:55 +0200

    Initial commit

---
+++ starter-kit/src/data/navigation/horizontalMenuData.tsx
@@ -0,0 +1,72 @@
+// Type Imports
+import type { HorizontalMenuDataType } from '@/types/menuTypes'
+
+const horizontalMenuData = (): HorizontalMenuDataType[] => [
+  {
+    label: 'Home',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Dashboard',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Provinces',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Districts',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Cities & Towns',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Agritext Officers',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Farmers',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Marketplace',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Blog',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Tools',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Settings',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Help',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'About',
+    href: '/about',
+    icon: 'tabler-info-circle'
+  }
+]
+
+export default horizontalMenuData
---
+++ starter-kit/src/data/navigation/verticalMenuData.tsx
@@ -0,0 +1,72 @@
+// Type Imports
+import type { VerticalMenuDataType } from '@/types/menuTypes'
+
+const verticalMenuData = (): VerticalMenuDataType[] => [
+  {
+    label: 'Home',
+    href: '/home',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Dashboard',
+    href: '/dashboard',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Provinces',
+    href: '/provinces',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Districts',
+    href: '/districts',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Cities & Towns',
+    href: '/towns',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Agritext Officers',
+    href: '/agritex',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Farmers',
+    href: '/farmers',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Marketplace',
+    href: '/marketplace',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Blog',
+    href: '/blog',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Tools',
+    href: '/tools',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Settings',
+    href: '/settings',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'Help',
+    href: '/help',
+    icon: 'tabler-smart-home'
+  },
+  {
+    label: 'About',
+    href: '/about',
+    icon: 'tabler-info-circle'
+  }
+]
+
+export default verticalMenuData
