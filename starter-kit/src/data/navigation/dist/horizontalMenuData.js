876548a381d5c6ed47f8d8c6dba871b7cbb623d5
Douglas Maposa <dovelous@gmail.com>
2024-11-28 21:48:14 +0200

    Update

--- starter-kit/src/data/navigation/dist/horizontalMenuData.js
+++ starter-kit/src/data/navigation/dist/horizontalMenuData.js
@@ -1,5 +1,7 @@
 "use strict";
+
 exports.__esModule = true;
+
 var horizontalMenuData = function () { return [
     {
         label: 'Home',
--- starter-kit/src/data/navigation/dist/verticalMenuData.js
+++ starter-kit/src/data/navigation/dist/verticalMenuData.js
@@ -1,5 +1,7 @@
 "use strict";
+
 exports.__esModule = true;
+
 var verticalMenuData = function () { return [
     {
         label: 'Home',
