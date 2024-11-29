889cc3246931f3e426922bba95d119d6d6f9dfee
Douglas Maposa <dovelous@gmail.com>
2024-11-29 07:37:50 +0200

    Cleaned footer

--- full-version/src/components/layout/vertical/FooterContent.tsx
+++ full-version/src/components/layout/vertical/FooterContent.tsx
@@ -21,7 +21,7 @@ const FooterContent = () => {
       className={classnames(verticalLayoutClasses.footerContent, 'flex items-center justify-between flex-wrap gap-4')}
     >
       <p>
-        <span className='text-textSecondary'>{`© ${new Date().getFullYear()}, Made with `}</span>
+        <span className='text-textSecondary'>{`© ${new Date().getFullYear()}, Made with xxx`}</span>
         <span>{`❤️`}</span>
         <span className='text-textSecondary'>{` by `}</span>
         <Link href='https://pixinvent.com' target='_blank' className='text-primary uppercase'>
--- starter-kit/src/components/layout/vertical/FooterContent.tsx
+++ starter-kit/src/components/layout/vertical/FooterContent.tsx
@@ -21,29 +21,29 @@ const FooterContent = () => {
       className={classnames(verticalLayoutClasses.footerContent, 'flex items-center justify-between flex-wrap gap-4')}
     >
       <p>
-        <span className='text-textSecondary'>{`© ${new Date().getFullYear()}, Made with `}</span>
+        <span className='text-textSecondary'>{`© ${new Date().getFullYear()}, Made with`}</span>
         <span>{`❤️`}</span>
         <span className='text-textSecondary'>{` by `}</span>
-        <Link href='https://pixinvent.com' target='_blank' className='text-primary uppercase'>
-          Pixinvent
+        <Link href='https://google.com' target='_blank' className='text-primary uppercase'>
+          MITTO
         </Link>
       </p>
       {!isBreakpointReached && (
         <div className='flex items-center gap-4'>
-          <Link href='https://themeforest.net/licenses/standard' target='_blank' className='text-primary'>
+          <Link href='#' target='_blank' className='text-primary'>
             License
           </Link>
-          <Link href='https://themeforest.net/user/pixinvent/portfolio' target='_blank' className='text-primary'>
-            More Themes
+          <Link href='#' target='_blank' className='text-primary'>
+            Help
           </Link>
           <Link
-            href='https://demos.pixinvent.com/vuexy-nextjs-admin-template/documentation'
+            href='#'
             target='_blank'
             className='text-primary'
           >
             Documentation
           </Link>
-          <Link href='https://pixinvent.ticksy.com' target='_blank' className='text-primary'>
+          <Link href='#' target='_blank' className='text-primary'>
             Support
           </Link>
         </div>
